2aa990b2e11e933e09039ece923b3f56244edd6d
momoky <778259229@qq.com>
2022-09-03 18:53:26 +0800

    update: 重写主应用

---
+++ examples/apps/main/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <el-config-provider :locale="zhCn">
+    <router-view />
+  </el-config-provider>
+</template>
+
+<script lang="ts" setup>
+import { watch } from 'vue'
+import { useRoute } from 'vue-router'
+import { useSetting } from '@/store'
+import zhCn from 'element-plus/lib/locale/lang/zh-cn'
+
+const route = useRoute()
+const setting = useSetting()
+
+watch(
+  route,
+  () => {
+    document.title = setting.title + '-' + route.meta.title
+  },
+  { immediate: true },
+)
+</script>
+
+<script lang="ts">
+export default {
+  name: 'App',
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  width: 100%;
+  min-height: 100vh;
+}
+</style>
